cb4694b40c51758dc4a1595d931bbe4a51dc25f5
yo-yo <107567806+01Yo-yo01@users.noreply.github.com>
2023-10-23 01:27:11 -0300

    nada pra fazer
    sla??

---
+++ slaa.js
@@ -0,0 +1,9 @@
+var audio = document.getElementById('audio');
+var playButton = documente.getElementById('playButton');
+var text = document.getElementById("text")
+
+function olhaOPorco() {
+    audio.play();
+    text.textContent = "É PEGADINHA POWW KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK";
+    playButton.style.display = "none";
+}
